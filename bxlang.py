45c84f63d9aa39309c51adf084733a17b572b5a9
sdaf47 <sdaf47@yandex.ru>
2017-03-04 00:56:02 +0300

    update

--- bxlang.py
+++ bxlang.py
@@ -10,6 +10,9 @@ component = sys.argv[-1]
 isTemplate = '-t' in sys.argv
 
 # TODO запятые, точки, английский. Искать в тегах и в кавычках, многострочно.
+# TODO проверка ланг-файлов на уникальность.
+# TODO укорачивание названий.
+# TODO Если файл уже существует, то просто дописываем в него
 charMap = maketrans(
     'йцукенгшщзхъфывапролджэячсмитьбюё,. ',
     'ICUKENGSSZHBFYWAPROLDZEYCSMITPBUE___'
@@ -28,45 +31,113 @@ template_map = (
     'component_epilog'
 )
 
+
+def create_lang_file(directory, name, messages):
+    lang_dir = directory + 'lang/ru/'
+    lang_template = '$MESS["{}"] = "{}";\n'
+
+    if not os.path.isdir(directory + 'lang'):
+        if not os.path.isdir(directory + 'lang'):
+            os.mkdir(directory + 'lang')
+        os.mkdir(lang_dir)
+
+    lang_file = open(lang_dir + name + '.php', 'w+')
+    lang_file.write('<?php\n')
+    langs = []
+    for message in messages:
+        lang_o = {
+            'code': name.upper().replace('.', '') + '_' + message.lower().translate(charMap),
+            'message': message,
+        }
+
+        lang_file.write(lang_template.format(
+            lang_o['code'],
+            lang_o['message']
+        ))
+        langs.append(lang_o)
+
+    return langs
+
+
+def find_lang(code):
+    return re.findall(r'[а-яА-Я]+[\s,().-_а-яА-Я]+', code)
+
+
+def replace_component_lang(langs, code):
+    for lang in langs:
+        pass
+        text_template = "\Bitrix\Main\Localization\Loc::getMessage(\"{}\")"
+        code = code.replace("\"" + lang['message'] + "\"",  text_template.format(lang['code']))
+        code = code.replace("\'" + lang['message'] + "\'", text_template.format(lang['code']))
+        code = code.replace(lang['message'], text_template.format(lang['code']))
+
+    return code
+
+
+def replace_template_lang(langs, code):
+    for lang in langs:
+        pass
+        quote_template = "\Bitrix\Main\Localization\Loc::getMessage(\"{}\")"
+        text_template = "<? \Bitrix\Main\Localization\Loc::getMessage(\"{}\") ?>"
+        code = code.replace("\"" + lang['message'] + "\"",  quote_template.format(lang['code']))
+        code = code.replace("\'" + lang['message'] + "\'", quote_template.format(lang['code']))
+        code = code.replace(lang['message'], text_template.format(lang['code']))
+
+    return code
+
 if "/" not in component:
-    componentPath = component + "/"
+    component_path = component + "/"
 else:
-    componentPath = component
+    component_path = component
 
 print('Component: ', component[:-1])
 
 for file_name in component_map:
     try:
-        file = open(componentPath + file_name + '.php')
+        file = open(component_path + file_name + '.php')
     except FileNotFoundError:
         continue
 
-    code = file.read()
-    messages = re.findall(r'[а-яА-Я]+[\s,.-_а-яА-Я]+', code)
+    php_code = file.read()
 
-    lang_dir = componentPath + 'lang/ru/'
-    if not os.path.isdir(lang_dir):
-        os.mkdir(lang_dir)
+    text_messages = find_lang(php_code)
 
-    lang_file = open(lang_dir + file_name + '.php', 'w+')
-    lang_file.write('<?php\n')
-    lang_list = []
-    for message in messages:
-        lang = {
-            'code': file_name.upper().replace('.', '') + '_' + message.lower().translate(charMap),
-            'message': message,
-        }
-        lang_file.write('$MESS["' + lang['code'] + '"] = "' + lang['message'] + '";\n')
-        lang_list.append(lang)
+    lang_list = create_lang_file(component_path, file_name, text_messages)
 
-    for lang in lang_list:
-        code = code.replace("\"" + lang['message'] + "\"", "\Bitrix\Main\Localization\Loc::getMessage(\"" + lang['code'] + "\")")
-        code = code.replace("\'" + lang['message'] + "\'", "\Bitrix\Main\Localization\Loc::getMessage(\"" + lang['code'] + "\")")
-        code = code.replace(lang['message'], "\Bitrix\Main\Localization\Loc::getMessage(\"" + lang['code'] + "\")")
+    php_code = replace_component_lang(lang_list, php_code)
 
     file.close()
 
-    file = open(componentPath + file_name + '.php', 'w+')
-    file.write(code)
+    file = open(component_path + file_name + '.php', 'w+')
+    file.write(php_code)
 
     file.close()
+
+templates = os.listdir(component_path + 'templates/')
+
+for template in templates:
+    template_path = component_path + 'templates/' + template + '/'
+
+    if not os.path.isdir(template_path):
+        continue
+
+    for file_name in template_map:
+        try:
+            file = open(template_path + file_name + '.php')
+        except FileNotFoundError:
+            continue
+
+        php_code = file.read()
+
+        text_messages = find_lang(php_code)
+
+        lang_list = create_lang_file(template_path, file_name, text_messages)
+
+        php_code = replace_template_lang(lang_list, php_code)
+
+        file.close()
+
+        file = open(template_path + file_name + '.php', 'w+')
+        file.write(php_code)
+
+        file.close()
