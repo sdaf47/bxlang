81c130c04cc741b2f3ff83be994c1a1b70594368
sdaf47 <sdaf47@yandex.ru>
2017-03-20 13:44:20 +0300

    install file

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp -R ../lang-parser/ ~/bin/lang-parser
+ln -s ~/bin/lang-parser/langparser.py ~/bin/bxlang
+chmod +x bxlang
--- langparser.py
+++ langparser.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.5
 import sys
 
 from langparser.strategy import StrategyParser
